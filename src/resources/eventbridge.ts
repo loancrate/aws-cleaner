ef2f651b53b9e83a37d6e338ce1a27ec78879de5
Trevor Robinson <trevor@loancrate.com>
2023-04-10 16:16:16 -0500

    Remove EventBridge rule targets

--- src/resources/eventbridge.ts
+++ src/resources/eventbridge.ts
@@ -1,4 +1,9 @@
-import { DeleteRuleCommand, EventBridgeClient } from "@aws-sdk/client-eventbridge";
+import {
+  DeleteRuleCommand,
+  EventBridgeClient,
+  ListTargetsByRuleCommand,
+  RemoveTargetsCommand,
+} from "@aws-sdk/client-eventbridge";
 import { ResourceDestroyerParams } from "../ResourceDestroyer.js";
 
 let client: EventBridgeClient | undefined;
@@ -13,9 +18,33 @@ function getClient(): EventBridgeClient {
 export async function deleteEventBridgeRule({
   resourceId,
 }: Pick<ResourceDestroyerParams, "resourceId">): Promise<void> {
+  const targetIds = await listTargets(resourceId);
+  await removeTargets(resourceId, targetIds);
+
   const client = getClient();
   const command = new DeleteRuleCommand({
     Name: resourceId,
+  });
+  await client.send(command);
+}
+
+async function listTargets(ruleId: string): Promise<string[]> {
+  const client = getClient();
+  const command = new ListTargetsByRuleCommand({
+    Rule: ruleId,
+  });
+  const response = await client.send(command);
+  if (response.Targets) {
+    return response.Targets.map((target) => target.Id) as string[];
+  }
+  return [];
+}
+
+async function removeTargets(ruleId: string, targetIds: string[]): Promise<void> {
+  const client = getClient();
+  const command = new RemoveTargetsCommand({
+    Rule: ruleId,
+    Ids: targetIds,
   });
   await client.send(command);
 }
